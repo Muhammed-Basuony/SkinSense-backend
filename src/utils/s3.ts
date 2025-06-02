69f4e81b8e4073433c21e033978f53429c1ed857
Muhammed-Basuony <muhammedbasuony2000@gmail.com>
2025-06-02 06:09:06 +0300

    Updates

--- src/utils/s3.ts
+++ src/utils/s3.ts
@@ -14,7 +14,6 @@ export const s3Upload = multer({
   storage: multerS3({
     s3,
     bucket: process.env.S3_BUCKET_NAME!,
-    acl: "public-read",
     contentType: multerS3.AUTO_CONTENT_TYPE,
     metadata: (_req, file, cb) => {
       cb(null, { fieldName: file.fieldname });
