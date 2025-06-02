e349fb45cf8d530f92b9e9cf14232f899e13c4d1
Muhammed-Basuony <muhammedbasuony2000@gmail.com>
2025-06-02 05:39:56 +0300

    Updates

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -6,5 +6,5 @@ dotenv.config();
 const JWT_SECRET = process.env.JWT_SECRET || "super-secret-key";
 
 export function generateToken(payload: object): string {
-  return jwt.sign(payload, JWT_SECRET, { expiresIn: "1h" });
+  return jwt.sign(payload, JWT_SECRET, { expiresIn: "365d" });
 }
