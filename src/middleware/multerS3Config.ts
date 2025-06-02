9d9fb178037bf9c837847776aa6a2755b4cd1c13
Muhammed-Basuony <muhammedbasuony2000@gmail.com>
2025-06-02 16:53:57 +0300

    Updates

--- src/middleware/multerS3Config.ts
+++ src/middleware/multerS3Config.ts
@@ -17,8 +17,9 @@ const s3 = new S3Client({
 const s3Uploader = multer({
   storage: multerS3({
     s3,
-    bucket: process.env.S3_BUCKET_NAME!, 
-    acl: 'public-read',
+    bucket: process.env.AWS_S3_BUCKET!,
+  
+    contentType: multerS3.AUTO_CONTENT_TYPE,
     metadata: (_req: Request, file: Express.Multer.File, cb) => {
       cb(null, { fieldName: file.fieldname });
     },
